e3187b48c0f667f62d13010aed9110c3b5b77b32
Álef Ribeiro <alef.ribeiro@wlcsolucoes.com.br>
2025-05-30 17:02:09 -0300

    Criação componente Container

---
+++ src/components/Container.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps, ReactNode } from "react";
+
+interface ContairnerProps extends ComponentProps<"div"> {
+    children: ReactNode;
+}
+
+export function Container({ children, className }: ContairnerProps) {
+    return (
+        <div className={cn("shadow-default rounded-sm py-4 px-5", className)}>
+            {children}
+        </div>
+    );
+}
