7206ebc50099f15860a99ab5204c92e527f5067c
Álef Ribeiro <alef.ribeiro@wlcsolucoes.com.br>
2025-05-30 16:11:00 -0300

    Instalação shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+    function handleClick() {
+        console.log("Você clicou no botão!");
+    }
     return (
         <div className="">
-            <h1>Home</h1>
+            <Button onClick={handleClick}>Me clique.</Button>
         </div>
     );
 }
